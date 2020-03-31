2042d282d559899590ce0fc78d94a43546b3857d
Roshan Lasiwa <roshanlasiwa@gmail.com>
2020-03-31 16:50:04 +0545

    added component and formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { TodosItemComponent } from './components/todos-item/todos-item.component';
-import { HeaderComponent } from './components/layout/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodoComponent } from "./components/todo/todo.component";
+import { TodosItemComponent } from "./components/todos-item/todos-item.component";
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { AddTodoComponent } from "./components/add-todo/add-todo.component";
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodosItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
