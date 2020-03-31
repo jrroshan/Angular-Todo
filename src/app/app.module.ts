ac2c8e80b461c9ee70f35a865f25d55d92833f02
Roshan Lasiwa <roshanlasiwa@gmail.com>
2020-03-31 15:45:11 +0545

    headercomponent Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodosItemComponent } from './components/todos-item/todos-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    TodosItemComponent
+    TodosItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
